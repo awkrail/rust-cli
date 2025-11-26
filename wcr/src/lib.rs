9a6b1b6ebb44f72dbcb78bf313645dcb3315ea9c
awkrail <taichitary@gmail.com>
2025-11-26 20:22:27 +0900

    implemented wcr

--- wcr/src/lib.rs
+++ wcr/src/lib.rs
@@ -28,6 +28,19 @@ pub fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
     let mut num_words = 0;
     let mut num_bytes = 0;
     let mut num_chars = 0;
+    let mut line = String::new();
+
+    loop {
+        let line_bytes = file.read_line(&mut line)?;
+        if line_bytes == 0 {
+            break;
+        }
+        num_bytes += line_bytes;
+        num_lines += 1;
+        num_words += line.split_whitespace().count();
+        num_chars += line.chars().count();
+        line.clear();
+    }
 
     Ok(FileInfo {
         num_lines,
@@ -37,13 +50,59 @@ pub fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
     })
 }
 
+
+fn format_field(value: usize, show: bool) -> String {
+    if show {
+        format!("{:>8}", value)
+    } else {
+        "".to_string()
+    }
+}
+
+
+
 pub fn run(config: Config) -> MyResult<()> {
+    let mut total_lines = 0;
+    let mut total_words = 0;
+    let mut total_bytes = 0;
+    let mut total_chars = 0;
+
     for filename in &config.files {
         match open(filename) {
             Err(err) => eprintln!("{}: {}", filename, err),
-            Ok(_) => println!("Opened {}", filename),
+            Ok(file) => {
+                if let Ok(info) = count(file) {
+                    println!(
+                        "{}{}{}{}{}",
+                        format_field(info.num_lines, config.lines),
+                        format_field(info.num_words, config.words),
+                        format_field(info.num_bytes, config.bytes),
+                        format_field(info.num_chars, config.chars),
+                        if filename == "-" {
+                            "".to_string()
+                        } else {
+                            format!(" {}", filename)
+                        }
+                    );
+                    
+                    total_lines += info.num_lines;
+                    total_words += info.num_words;
+                    total_bytes += info.num_bytes;
+                    total_chars += info.num_chars;
+                }
+            }
         }
     }
+    
+    if config.files.len() > 1 {
+        println!("{}{}{}{} total",
+            format_field(total_lines, config.lines),
+            format_field(total_words, config.words),
+            format_field(total_bytes, config.bytes),
+            format_field(total_chars, config.chars),
+        );
+    }
+
     Ok(())
 }
 
@@ -70,6 +129,7 @@ pub fn get_args() -> MyResult<Config> {
             Arg::with_name("lines")
                 .value_name("LINES")
                 .short("l")
+                .long("lines")
                 .takes_value(false)
                 .help("The number of lines"),
         )
@@ -77,6 +137,7 @@ pub fn get_args() -> MyResult<Config> {
             Arg::with_name("words")
                 .value_name("WORDS")
                 .short("w")
+                .long("words")
                 .takes_value(false)
                 .help("The number of words"),
         )
@@ -84,12 +145,14 @@ pub fn get_args() -> MyResult<Config> {
             Arg::with_name("bytes")
                 .value_name("BYTES")
                 .short("c")
+                .long("bytes")
                 .takes_value(false)
                 .help("The number of bytes"),
         ).arg(
             Arg::with_name("chars")
                 .value_name("CHARS")
                 .short("m")
+                .long("chars")
                 .takes_value(false)
                 .conflicts_with("bytes")
                 .help("The number of characters"),
