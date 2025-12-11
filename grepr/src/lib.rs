cf8f2fa51d2e9e7de56e43ba19437ae125988bae
awkrail <taichitary@gmail.com>
2025-12-11 20:01:43 +0900

    fix

--- grepr/src/lib.rs
+++ grepr/src/lib.rs
@@ -1,6 +1,8 @@
 use clap::{App, Arg};
 use regex::{Regex, RegexBuilder};
 use std::error::Error;
+use std::path::Path;
+use std::fs;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -68,11 +70,38 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 fn find_files(paths: &[String], recursive: bool) -> Vec<MyResult<String>> {
-    unimplemented!();
+    let mut results: Vec<MyResult<String>> = vec![];
+    for path in paths {
+        let path = Path::new(path);
+        if path.is_file() {
+            results.push(Ok(path.to_str().unwrap().to_string()));
+        } else {
+            if !recursive {
+                results.push(Err(format!("{} is a directory", path.to_str().unwrap().to_string()).into()));
+            }
+            else {
+                let entries = fs::read_dir(path)
+                    .unwrap()
+                    .map(|res| {
+                        let entry = res.unwrap();
+                        entry.path()
+                           .to_string_lossy()
+                           .into_owned()
+                    })
+                    .collect::<Vec<String>>();
+                
+                for entry in entries {
+                    results.push(Ok(entry));
+                }
+            }
+        }
+    }
+    results
 }
 
 pub fn run(config: Config) -> MyResult<()> {
     println!("{:#?}", config);
+    find_files(&config.files, false);
     Ok(())
 }
 
@@ -83,42 +112,45 @@ mod tests {
 
     #[test]
     fn test_find_files() {
-        let files = find_files(&["./test/inputs/fox.txt".to_string()], false);
+        let files = find_files(&["./tests/inputs/fox.txt".to_string()], false);
         assert_eq!(files.len(), 1);
         assert_eq!(files[0].as_ref().unwrap(), "./tests/inputs/fox.txt");
 
         // recursive
+        let files = find_files(&["./tests/inputs".to_string()], false);
+        assert_eq!(files.len(), 1);
+        if let Err(e) = &files[0] {
+            assert_eq!(e.to_string(), "./tests/inputs is a directory");
+        }
 
+        let res = find_files(&["./tests/inputs".to_string()], true);
+        let mut files: Vec<String> = res
+            .iter()
+            .map(|r| r.as_ref().unwrap().replace("\\", "/"))
+            .collect();
 
+        files.sort();
+        assert_eq!(files.len(), 4);
+        assert_eq!(
+            files,
+            vec![
+                "./tests/inputs/bustle.txt",
+                "./tests/inputs/empty.txt",
+                "./tests/inputs/fox.txt",
+                "./tests/inputs/nobody.txt",
+            ]
+        );
 
+        let bad : String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect();
 
-
-
-
+        let files = find_files(&[bad], false);
+        assert_eq!(files.len(), 1);
+        assert!(files[0].is_err());
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 
